b0c7b94e1808915bfc5e98cf4f2d8729637b8165
4c74356b41 <spam@4c74356b41.com>
2017-09-19 11:54:19 +0300

    Update #profile.ps1

--- #profile.ps1
+++ #profile.ps1
@@ -14,6 +14,22 @@ function develop-me {
 	Invoke-RestMethod -Method Post -Uri $webhook -Body $whbody
 }
 
+function git-me {
+	git config --global user.email "4c74356b41@outlook.com"
+	git config --global user.name "Gleb Boushev"
+}
+
+function share-me {
+	Param(
+	[string]$storageName,
+	[string]$storagePass
+	)
+	$acctKey = ConvertTo-SecureString -String $storagePass -AsPlainText -Force
+	$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$storageName", $acctKey
+	New-PSDrive -Name z -PSProvider FileSystem -Root "\\$storageName.file.core.windows.net\bylya" -Credential $credential -Persist
+	New-PSDrive -Name x -PSProvider FileSystem -Root "\\$storageName.file.core.windows.net\nebylya" -Credential $credential -Persist
+}
+
 function get-image {
 	Param(
         [string]$pub,
