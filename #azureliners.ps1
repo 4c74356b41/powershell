570dd04166dc38b4213a608bea8120bb7cbc42c2
4c74356b41 <spam@4c74356b41.com>
2019-01-08 21:33:15 +0300

    Update #azureliners.ps1

--- #azureliners.ps1
+++ #azureliners.ps1
@@ -1,29 +1,29 @@
 # get provider api-versions
-(Get-AzureRmResourceProvider -ProviderNamespace 'Microsoft.Insights').ResourceTypes | FT ResourceTypeName, ApiVersions
+(Get-AzResourceProvider -ProviderNamespace 'Microsoft.Insights').ResourceTypes | FT ResourceTypeName, ApiVersions
 
 # get all operations
-$ops = (Get-AzureRmProviderOperation -OperationSearchString */*).Operation
+$ops = (Get-AzProviderOperation -OperationSearchString */*).Operation
 
 # specifics (https://resource.azure.com)
-Get-AzureRmProviderOperation -OperationSearchString Microsoft.Cdn/* | Where { $_.Operation -like '*action' } | Format-Table 
-Invoke-AzureRmResourceAction -ResourceGroupName $rg -ResourceType 'Microsoft.Cdn/profiles/endpoints' -ResourceName $ProfileName/$EndpointName `
+Get-AzProviderOperation -OperationSearchString Microsoft.Cdn/* | Where { $_.Operation -like '*action' } | Format-Table 
+Invoke-AzResourceAction -ResourceGroupName $rg -ResourceType 'Microsoft.Cdn/profiles/endpoints' -ResourceName $ProfileName/$EndpointName `
     -ApiVersion '2015-06-01' -Action 'Purge' -Parameters @{ ContentPaths = '/*' } -Force
 
 # "advanced" shroubletooting
-$logentry = Get-AzureRMLog -CorrelationId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -DetailedOutput
+$logentry = Get-AzLog -CorrelationId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -DetailedOutput
 $rawStatusMessage = $logentry.Properties
 $status = $rawStatusMessage.Content.statusMessage | ConvertFrom-Json
 
 # providerz
-Get-AzureRmResourceProvider -ListAvailable | select ProviderNamespace
-Get-AzureRmResourceProvider -ListAvailable | where{$_.ProviderNamespace -like "*compute"} | foreach-object{Register-AzureRmResourceProvider -ProviderNamespace $_.ProviderNamespace}
+Get-AzResourceProvider -ListAvailable | select ProviderNamespace
+Get-AzResourceProvider -ListAvailable | where{$_.ProviderNamespace -like "*compute"} | foreach-object{Register-AzResourceProvider -ProviderNamespace $_.ProviderNamespace}
 
 # new role
-$role = Get-AzureRmRoleDefinition "Virtual Machine Contributor"
+$role = Get-AzRoleDefinition "Virtual Machine Contributor"
 $role.Id = $null
 $role.Name = "Classic storage reader"
 $role.Actions.Clear()
 $role.Actions.Add("Microsoft.ClassicStorage/storageAccounts/read")
 $role.AssignableScopes.Clear()
 $role.AssignableScopes.Add("/subscriptions/xxxx")
-New-AzureRmRoleDefinition -Role $role
+New-AzRoleDefinition -Role $role
