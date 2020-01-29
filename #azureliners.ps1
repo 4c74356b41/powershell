c22df6e3119be470bebd8dfa78148b0fa3630d2a
4c74356b41 <spam@4c74356b41.com>
2020-01-29 19:54:48 +0300

    Update #azureliners.ps1

--- #azureliners.ps1
+++ #azureliners.ps1
@@ -1,29 +1,21 @@
 # get provider api-versions
-(Get-AzResourceProvider -ProviderNamespace 'Microsoft.Insights').ResourceTypes | Format-Table ResourceTypeName, ApiVersions
+( Get-AzResourceProvider -ProviderNamespace 'Microsoft.Insights' ).ResourceTypes | ft ResourceTypeName, ApiVersions
 
 # get all operations
-$ops = (Get-AzProviderOperation -OperationSearchString */*).Operation
+$ops = ( Get-AzProviderOperation -OperationSearchString '*/*' ).Operation
 
 # specifics (https://resource.azure.com)
-Get-AzProviderOperation -OperationSearchString Microsoft.Cdn/* | Where { $_.Operation -like '*action' } | Format-Table 
-Invoke-AzResourceAction -ResourceGroupName $rg -ResourceType 'Microsoft.Cdn/profiles/endpoints' -ResourceName $ProfileName/$EndpointName `
-    -ApiVersion '2015-06-01' -Action 'Purge' -Parameters @{ ContentPaths = '/*' } -Force
+Get-AzProviderOperation -OperationSearchString 'Microsoft.Cdn/*' | ? { $_.Operation -match 'action$' } | ft 
+Invoke-AzResourceAction -ResourceGroupName $rg -ResourceType 'Microsoft.Cdn/profiles/endpoints' `
+    -ResourceName $ProfileName/$EndpointName -Force -Action 'Purge' `
+    -ApiVersion '2015-06-01' -Parameters @{ ContentPaths = '/*' }
 
 # "advanced" shroubletooting
-$logentry = Get-AzLog -CorrelationId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -DetailedOutput
+$logentry = Get-AzLog -CorrelationId $guid -DetailedOutput
 $rawStatusMessage = $logentry.Properties
 $status = $rawStatusMessage.Content.statusMessage | ConvertFrom-Json
 
 # providerz
 Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace
-Get-AzResourceProvider -ListAvailable | Where-Object { $_.ProviderNamespace -match "compute" } | Foreach-Object { Register-AzResourceProvider -ProviderNamespace $_.ProviderNamespace }
-
-# new role
-$role = Get-AzRoleDefinition "Virtual Machine Contributor"
-$role.Id = $null
-$role.Name = "Classic storage reader"
-$role.Actions.Clear()
-$role.Actions.Add("Microsoft.ClassicStorage/storageAccounts/read")
-$role.AssignableScopes.Clear()
-$role.AssignableScopes.Add("/subscriptions/xxxx")
-New-AzRoleDefinition -Role $role
+Get-AzResourceProvider -ListAvailable | Where-Object { $_.ProviderNamespace -match "compute" }
+    | Foreach-Object { Register-AzResourceProvider -ProviderNamespace $_.ProviderNamespace }
