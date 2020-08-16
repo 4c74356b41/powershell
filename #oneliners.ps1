a29b8f05cdb2a62775abded3ba5ca777caff37af
4c74356b41 <spam@4c74356b41.com>
2020-08-16 09:57:41 +0300

    Update #oneliners.ps1

--- #oneliners.ps1
+++ #oneliners.ps1
@@ -1,15 +1,17 @@
 # initial setup
 set-executionpolicy unrestricted
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y kubernetes-cli kubernetes-helm istioctl openshift-cli 
+choco install -y kubernetes-cli kubernetes-helm istioctl openshift-cli fluxctl
 choco install -y git 7zip vscode vlc microsoft-teams slack telegram
 if (!(Invoke-RestMethod -Headers @{"Metadata"="true"} 'http://169.254.169.254/metadata/instance?api-version=2018-10-01')) {
     choco install -y docker-desktop
 }
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 install-module posh-git,mvp,az,az.resourcegraph
-git config --global user.email "4c74356b41@outlook.com"
+git config --global user.email "core@4c74356b41.com"
 git config --global user.name "Gleb Boushev"
+git config --global core.eol lf
+git config --global core.autocrlf input
 'Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/4c74356b41/powershell/master/%23profile.ps1")' > $profile
 
 #creds\session
