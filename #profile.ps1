e39d3b1a52a29fc550e9475166adea99a0902944
Gleb Boushev <spam@4c74356b41.com>
2021-07-06 08:53:04 +0300

    Update #profile.ps1

--- #profile.ps1
+++ #profile.ps1
@@ -261,7 +261,7 @@ function workhour-me ([switch]$previous, $modifier = 1) {
     ( 1..[DateTime]::DaysInMonth( $now.Year, $month) ).where{
         ( Get-Date -Day $_ ).DayOfWeek -in 1..5 }.count * 8 * $modifier
 }
-function base64-file-me ($b64, $filename) {
+function base64-file-me ($b64, $filename = "$home\b64.temp") {
     $bytes = [Convert]::FromBase64String($b64)
     [IO.File]::WriteAllBytes($filename, $bytes)
 }
