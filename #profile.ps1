9bae3dd1b0a7289658bff7c5f851ae5b2886edaf
Gleb Boushev <spam@4c74356b41.com>
2021-12-08 05:32:12 -0800

    Update #profile.ps1

--- #profile.ps1
+++ #profile.ps1
@@ -257,7 +257,7 @@ $GitPromptSettings.DefaultPromptSuffix.Text = ""
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
 $GitPromptSettings.DefaultPromptPath.Text = '$( ( Get-PromptPath ) -replace "C:\\Users\\core\\onedrive\\_git","~" )'
 $PSDefaultParameterValues["Out-Default:OutVariable"] = "lw"
-New-BashStyleAlias gc 'git commit @args'
+New-BashStyleAlias gtc 'git commit @args'
 New-BashStyleAlias gpf 'git pull --ff-only @args'
 New-BashStyleAlias gfa 'git fetch --all --prune @args'
 
