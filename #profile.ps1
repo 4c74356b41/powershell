3f821df7a87bbbd35521caca2e30d837f9368b35
Gleb Boushev <spam@4c74356b41.com>
2025-07-21 10:01:27 +0400

    Update #profile.ps1

--- #profile.ps1
+++ #profile.ps1
@@ -214,13 +214,21 @@ function workhour-me ([int]$offset, $hours = 8) {
     ( Get-Date -Day $_ -Month $month ).DayOfWeek -in 1..5
   }.count * $hours
 }
-function base64-file-me ($b64, $filename = "b64.temp", $print = $true) {
-  $bytes = [Convert]::FromBase64String($b64)
-  [IO.File]::WriteAllBytes("$pwd/$filename", $bytes)
-  if ($print) {
-    [System.Text.Encoding]::UTF8.GetString($bytes)
+
+function base64-file-me ($b64, $filename = "b64.temp", $print = $true, $encode = $false) {
+  if ($encode) {
+    $fileBytes = [System.IO.File]::ReadAllBytes("$pwd/$filename")
+    [Convert]::ToBase64String($fileBytes)
+  } else {
+    $bytes = [Convert]::FromBase64String($b64)
+    if ($print) {
+      [System.Text.Encoding]::UTF8.GetString($bytes)
+    } else {
+      [IO.File]::WriteAllBytes("$pwd/$filename", $bytes)
+    }
   }
 }
+
 function hosts-me {
   code C:\Windows\System32\drivers\etc\hosts
 }
